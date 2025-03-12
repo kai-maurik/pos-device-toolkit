e3251d5d749a90cdd2b3a4becb49137b2a49e7db
Kai van Maurik <kai.maurik@hotmail.com>
2025-03-12 20:50:00 +0100

    Bugfix: Now wait for Mozzila to name browser

--- pdt@kaivanmaurik.com/extension.js
+++ pdt@kaivanmaurik.com/extension.js
@@ -15,67 +15,54 @@ class Extension {
         console.log(`pdt: constructing ${Me.metadata.name}`);
     }
 
-    /**
-     * This function is called when your extension is enabled, which could be
-     * done in GNOME Extensions, when you log in or when the screen is unlocked.
-     *
-     * This is when you should setup any UI for your extension, change existing
-     * widgets, connect signals or modify GNOME Shell's behavior.
-     */
     enable() {
         console.log(`pdt: enabling ${Me.metadata.name}`);
         last_window_id_list = global.get_window_actors().map(w => w.meta_window.get_id());
         
-        global.window_change_signal = global.display.connect('window-created', () => {
-            for(let i = 0; i < global.get_window_actors().length; i++) {
-                let window_actor = global.get_window_actors()[i];
-                let current_window_id_list = global.get_window_actors().map(w => w.meta_window.get_id());
-
-                if(last_window_id_list.includes(window_actor.meta_window.get_id()))
-                    continue;
-
-                if(!window_actor.meta_window || window_actor.meta_window.get_window_type() > 1) //0=normal & 1=desktop, 2=dock, etc.
-                    continue;
-
-                console.log("pdt: Window open registered: " + window_actor.meta_window.get_id());
-
-                // Bug3: Sometimes when closing windows throws error for this line. This only appears when popup menus happen. Ignore
-                waitForString(() => window_actor.meta_window.get_title()).then((result) => {
-                    console.log("pdt: Title identified: " + result)
-                    if(result.includes("CustomerFacingDisplay")) {
-                        // Customer display found in the window_actor
-                        console.log("pdt: CustomerFacingDisplay detected. Applying actions...");
-
-                        if(customerFacingDisplayWindowId != -1 && current_window_id_list.includes(customerFacingDisplayWindowId)) {
-                            for(let j = 0; j < global.get_window_actors().length; j++) {
-                                if(global.get_window_actors()[j].meta_window.get_id() == customerFacingDisplayWindowId)
-                                    global.get_window_actors()[j].kill(); // Care: untested
-                            }
-                        }
-
-                        if(global.display.get_n_monitors() > 1) {
-                            window_actor.meta_window.move_to_monitor(1); // Care: untested
-                            window_actor.meta_window.make_fullscreen();
-                        }
-                    }
-                }).catch();
-            }
-            
-            last_window_id_list = global.get_window_actors().map(w => w.meta_window.get_id());
-        });
+        global.window_change_signal = global.display.connect('window-created', windowCreateEvent);
     }
 
-
-    /**
-     * This function is called when your extension is uninstalled, disabled in
-     * GNOME Extensions or when the screen locks.
-     *
-     * Anything you created, modified or setup in enable() MUST be undone here.
-     * Not doing so is the most common reason extensions are rejected in review!
-     */
     disable() {
         console.log(`pdt: disabling ${Me.metadata.name}`);
     }
+}
+
+function windowCreateEvent()
+{
+    for (let i = 0; i < global.get_window_actors().length; i++) 
+    {
+        let window_actor = global.get_window_actors()[i];
+        let current_window_id_list = global.get_window_actors().map(w => w.meta_window.get_id());
+
+        if(last_window_id_list.includes(window_actor.meta_window.get_id()))
+            continue;
+
+        if(!window_actor.meta_window || window_actor.meta_window.get_window_type() > 1) //0=normal & 1=desktop, 2=dock, etc.
+            continue;
+
+        waitForString(() => window_actor.meta_window.get_title()).then((result) => {
+            console.log("pdt: Title identified: " + result);
+            if (result.includes("CustomerFacingDisplay")) 
+            {
+                console.log("pdt: CustomerFacingDisplay detected. Applying actions...");
+                if(customerFacingDisplayWindowId != -1 && current_window_id_list.includes(customerFacingDisplayWindowId))
+                {
+                    for(let j = 0; j < global.get_window_actors().length; j++) 
+                    {
+                        if(global.get_window_actors()[j].meta_window.get_id() == customerFacingDisplayWindowId)
+                            global.get_window_actors()[j].kill();
+                    }
+                }
+                if(global.display.get_n_monitors() > 1)
+                {
+                    window_actor.meta_window.move_to_monitor(1);
+                    window_actor.meta_window.make_fullscreen();
+                }
+            }
+        }).catch();
+    }
+    
+    last_window_id_list = global.get_window_actors().map(w => w.meta_window.get_id());
 }
 
 function waitForString(getString, interval = 100) {
@@ -88,7 +75,7 @@ function waitForString(getString, interval = 100) {
         try {
             const checkInterval = setInterval(() => {
                 let value = getString();
-                if (typeof value === "string" && value.trim() !== "") {
+                if (typeof value === "string" && value.trim() !== "" && value !== "Mozilla Firefox") {
                     clearInterval(checkInterval);
                     resolve(value);
                 }
@@ -101,12 +88,6 @@ function waitForString(getString, interval = 100) {
 }
 
 /**
- * This function is called once when your extension is loaded, not enabled. This
- * is a good time to setup translations or anything else you only do once.
- *
- * You MUST NOT make any changes to GNOME Shell, connect any signals or add any
- * MainLoop sources here.
- *
  * @param {ExtensionMeta} meta - An extension meta object
  * @returns {object} an object with enable() and disable() methods
  */
