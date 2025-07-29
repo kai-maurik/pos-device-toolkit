91fde6f2a1047c54b6f329e18f4b2b8c969fa917
Kai van Maurik <kai.maurik@hotmail.com>
2025-07-29 11:59:53 +0200

    Code optimisations

--- pdt@kaivanmaurik.com/extension.js
+++ pdt@kaivanmaurik.com/extension.js
@@ -7,80 +7,101 @@
 const ExtensionUtils = imports.misc.extensionUtils;
 const Me = ExtensionUtils.getCurrentExtension();
 
-let last_window_id_list;
-let customerFacingDisplayWindowId = -1;
-
 class Extension {
-    constructor() {
+
+    constructor() 
+    {
         console.log(`pdt: constructing ${Me.metadata.name}`);
     }
 
     enable() {
-        console.log(`pdt: enabling ${Me.metadata.name}`);
+        let last_window_id_list;
+        let customerFacingDisplayWindowId;
+
         last_window_id_list = global.get_window_actors().map(w => w.meta_window.get_id());
+        customerFacingDisplayWindowId = -1;
+
+        console.log(`pdt: enabling ${Me.metadata.name}`);
+        global.window_change_signal = global.display.connect('window-created', () => 
+        {
+            for (let i = 0; i < global.get_window_actors().length; i++) 
+            {
+                let current_window_id_list;
+                let meta;
+
+                meta = global.get_window_actors()[i].meta_window;
+                current_window_id_list = global.get_window_actors().map(w => w.meta_window.get_id());
         
-        global.window_change_signal = global.display.connect('window-created', windowCreateEvent);
+                if(last_window_id_list.includes(meta.get_id()))
+                    continue;
+                if(!meta || meta.get_window_type() > 1) //0=normal & 1=desktop, 2=dock, etc.
+                    continue;
+                waitForString(() => {
+                    if(!meta || meta._destroyed) return "";
+                    else return meta.get_title();
+                }).then((result) => 
+                {
+                    console.log("pdt: Title identified: " + result);
+                    if (result.includes("CustomerFacingDisplay")) 
+                    {
+                        console.log("pdt: CustomerFacingDisplay detected."); 
+                        if(customerFacingDisplayWindowId != -1 && current_window_id_list.includes(customerFacingDisplayWindowId))
+                        {
+                            for(let j = 0; j < global.get_window_actors().length; j++)
+                            {
+                                if(global.get_window_actors()[j].meta_window.get_id() == customerFacingDisplayWindowId)
+                                    global.get_window_actors()[j].kill();
+                            }
+                        }
+                        if(global.display.get_n_monitors() > 1)
+                        {
+                            meta.move_to_monitor(1);
+                            meta.make_fullscreen();
+                            console.log("pdt: Applied actions succesfully.");
+                        }
+                        else
+                            console.log("pdt: No second monitor found. No actions applied.");
+                    }
+                }).catch(err => {
+                    console.log("pdt: Error while getting title: " + err.message);
+                });
+            }
+            last_window_id_list = global.get_window_actors().map(w => w.meta_window.get_id());
+        });
     }
 
     disable() {
+        if(global.window_change_signal) 
+        {
+            global.display.disconnect(global.window_change_signal);
+            global.window_change_signal = null;
+        }
         console.log(`pdt: disabling ${Me.metadata.name}`);
     }
 }
 
-function windowCreateEvent()
+function waitForString(getString, interval = 100) 
 {
-    for (let i = 0; i < global.get_window_actors().length; i++) 
-    {
-        let window_actor = global.get_window_actors()[i];
-        let current_window_id_list = global.get_window_actors().map(w => w.meta_window.get_id());
-
-        if(last_window_id_list.includes(window_actor.meta_window.get_id()))
-            continue;
-
-        if(!window_actor.meta_window || window_actor.meta_window.get_window_type() > 1) //0=normal & 1=desktop, 2=dock, etc.
-            continue;
-
-        waitForString(() => window_actor.meta_window.get_title()).then((result) => {
-            console.log("pdt: Title identified: " + result);
-            if (result.includes("CustomerFacingDisplay")) 
-            {
-                console.log("pdt: CustomerFacingDisplay detected. Applying actions...");
-                if(customerFacingDisplayWindowId != -1 && current_window_id_list.includes(customerFacingDisplayWindowId))
-                {
-                    for(let j = 0; j < global.get_window_actors().length; j++) 
-                    {
-                        if(global.get_window_actors()[j].meta_window.get_id() == customerFacingDisplayWindowId)
-                            global.get_window_actors()[j].kill();
-                    }
-                }
-                if(global.display.get_n_monitors() > 1)
-                {
-                    window_actor.meta_window.move_to_monitor(1);
-                    window_actor.meta_window.make_fullscreen();
-                }
-            }
-        }).catch();
-    }
-    
-    last_window_id_list = global.get_window_actors().map(w => w.meta_window.get_id());
-}
-
-function waitForString(getString, interval = 100) {
     return new Promise((resolve, reject) => {
-        if (typeof getString !== "function") {
+        if (typeof getString !== "function") 
+        {
             reject(new Error("Expected a function as the first argument."));
             return;
         }
 
         try {
-            const checkInterval = setInterval(() => {
+            const checkInterval = setInterval(() => 
+            {
                 let value = getString();
-                if (typeof value === "string" && value.trim() !== "" && value !== "Mozilla Firefox") {
+                if (typeof value === "string" && value.trim() !== "" && value !== "Mozilla Firefox") 
+                {
                     clearInterval(checkInterval);
                     resolve(value);
                 }
             }, interval);
-        } catch(error) {
+        } 
+        catch(error) 
+        {
             console.log("pdt: Window no longer exists. Returning empty title");
             resolve("Empty Title");
         }
